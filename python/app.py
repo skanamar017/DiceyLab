e34220e5db30ff93ad04ce60892eb5a9dea56ef7
skanamar017 <skanamar017@gmail.com>
2025-07-15 20:53:23 -0400

    Will make offical test cases soon

--- python/app.py
+++ python/app.py
@@ -5,7 +5,7 @@ class Bins:
         self.min_bin=min_bin
         self.max_bin=max_bin
         self.bins={}
-        for i in range(self.min_bin, self.max_bin):
+        for i in range(self.min_bin, self.max_bin+1):
             self.bins[i]=0
 
     def get_bin(self, bin_num: int):
@@ -19,14 +19,11 @@ class Bins:
 class Dice:
     def __init__(self, rolls: int):
         self.rolls=rolls
-    
-    def dice_roll(self):
-        return random.randrange(1, 6)
 
     def toss_and_sum(self):
         sum=0
         for i in range(self.rolls):
-            sum+=self.dice_roll()
+            sum+=random.randrange(1, 7)
         return sum
 
 
@@ -45,35 +42,50 @@ class Simulation:
             self.sim_bins.increment_bin(score)
         
         return self.sim_bins
+    
+    def print_results(self):
+        results=""
+        for num in self.sim_bins.bins:
+            results+=f"{num:>3} : {self.sim_bins.bins[num]:>9}: {round(self.sim_bins.bins[num]/self.numberOfTosses, 2):<4} {"*"*((self.sim_bins.bins[num]*100)//self.numberOfTosses)}\n"
+
+        return results
+
 
             
 
 
 
+def main():
+
+    craps=Dice(2)
+    craps_toss=craps.toss_and_sum()
+    print(craps_toss)
+
+    yatzee=Dice(5)
+    yatzee_toss=yatzee.toss_and_sum()
+    print(yatzee_toss)
+
+    results = Bins(2, 12)  # for bins from 2..12
+    print(results.bins)
+    #results.set_bins()
+    number_of_tens = results.get_bin(10)  # returns the number of tens in the 10 bin
+    print(number_of_tens)
+    results.increment_bin(10)  # should increment bin # 10
+    number_of_tens = results.get_bin(10)
+    print(number_of_tens)
+    print(results.bins)
 
 
-craps=Dice(2)
-craps_toss=craps.toss_and_sum()
-print(craps_toss)
+    print("simulation")
+    sim = Simulation(3, 1000000)
 
-yatzee=Dice(5)
-yatzee_toss=yatzee.toss_and_sum()
-print(yatzee_toss)
+    finals=sim.run_simulation()
 
-results = Bins(2, 12)  # for bins from 2..12
-print(results.bins)
-#results.set_bins()
-number_of_tens = results.get_bin(10)  # returns the number of tens in the 10 bin
-print(number_of_tens)
-results.increment_bin(10)  # should increment bin # 10
-number_of_tens = results.get_bin(10)
-print(number_of_tens)
-print(results.bins)
+    print(finals.bins)
 
+    printings=sim.print_results()
 
-print("simulation")
-sim = Simulation(2, 1000)
+    print(printings)
 
-finals=sim.run_simulation()
-
-print(finals.bins)
+if __name__ == "__main__":
+    main()
